8bb2a79117d1b368ab057679efbe5a14bc4412ba
Peter Bernath <93717649+pbernath@users.noreply.github.com>
2022-12-05 16:06:09 +0100

    done
    Co-Authored-By: snkth <99145257+snkth@users.noreply.github.com>

--- soundgoodQueries.sql
+++ soundgoodQueries.sql
@@ -55,31 +55,6 @@ HAVING COUNT(school_activity.activity_type_id) > 2
 ORDER BY COUNT(school_activity.activity_type_id) DESC
 
 
-THIRD QUERY (EXTENDED):
-
-SELECT
-	school_activity.instructor_id,
-	COUNT(school_activity.activity_type_id) AS total_lessons,
-	
-	COUNT(school_activity_id)
-		FILTER (WHERE school_activity.activity_type_id = 1)
-		AS individual,
-	
-	COUNT(school_activity_id)
-		FILTER (WHERE school_activity.activity_type_id = 2)
-		AS group,
-		
-	COUNT(school_activity_id)
-		FILTER (WHERE school_activity.activity_type_id = 3)
-		AS ensamble
-FROM school_activity
-WHERE extract (month FROM school_activity.date_and_time) = extract (month FROM CURRENT_DATE)
-GROUP BY school_activity.instructor_id  
-HAVING COUNT(school_activity.activity_type_id) > 2
-
-ORDER BY COUNT(school_activity.activity_type_id) DESC
-
-
 
 FOURTH QUERY
 
